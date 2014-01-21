ed2e0221e845b0551e9f062e6e7a7cf16ff034c7
Karthik Ram <karthik.ram@gmail.com>
2014-01-21 13:28:07 -0800

    ggmap work correctly with various occ results

--- R/mapggplot.R
+++ R/mapggplot.R
@@ -3,22 +3,33 @@
 #' @param df Input \code{data.frame}
 #' @import ggmap
 #' @export
+#' @param zoom zoom level for map. Adjust depending on how your data look.
+#' @param point_color Default color of your points
 #' @examples \dontrun{
-#' z <- ecoengine::ee_observations(scientific_name__exact = "Lynx rufus californicus", page = "all", georeferenced = TRUE)
-#' mapggplot(z$data)
+#' ecoengine_data <- occ(query = "Lynx rufus californicus", from = "ecoengine")
+#' mapggplot(ecoengine_data)
+#' gbif_data <- occ(query = 'Accipiter striatus', from = 'gbif')
+#' mapggplot(gbif_data)
 #'}
-mapggplot <- function(df) {
-	min_lat <- min(df$latitude)
-	max_lat <- max(df$latitude)
-	min_long <- min(df$longitude)
-	max_long <- max(df$longitude)
-	species <- unique(df$scientific_name)
+mapggplot <- function(df, zoom = 5, point_color = "#86161f") {
+	 dt <- occ2df(df)
+	# Remove rows with missing data
+	dt <- dt[complete.cases(dt), ]
+	min_lat <- min(dt$latitude, na.rm = TRUE)
+	max_lat <- max(dt$latitude, na.rm = TRUE)
+	min_long <- min(dt$longitude, na.rm = TRUE)
+	max_long <- max(dt$longitude, na.rm = TRUE)
+	species <- unique(dt$name)
 
 	center_lat <- min_lat + (max_lat - min_lat)/2
 	center_long <- min_long + (max_long - min_long)/2
 	 map_center <- c(lon = center_long, lat =  center_lat)
-	 species_map <- get_map(location = map_center, zoom = 8, maptype = "terrain", color = "bw")
-	 temp <- df[, c("latitude", "longitude")]
-	ggmap(species_map, extent = "panel", maprange = FALSE, fullpage = TRUE) + 
-	geom_point(data = temp, aes(x = longitude, y =latitude), color = "#F5C272", size = 2) + ggtitle(paste0("Distribution of ", species))
+	 species_map <- get_map(location = map_center, zoom = zoom, maptype = "terrain")
+	 temp <- dt[, c("latitude", "longitude")]
+	ggmap(species_map) + 
+	geom_point(data = temp, aes(x = longitude, y =latitude), color = point_color, size = 3) + 
+	ggtitle(paste0("Distribution of ", species)) + 
+	xlab("Longitude") + ylab("Latitude")
 } 
+# [BUGS]: Can't figure out why it leaves out points even after I center the plot
+# on the data. Setting zoom = "auto" leaves out even more points.

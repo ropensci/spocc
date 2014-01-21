28b21e717ee1f79e577ea0f4019b532b659f7118
sckott <myrmecocystus@gmail.com>
2014-01-21 10:04:27 -0800

    small change to occ to handle no data with rgbif calls

--- R/occ.r
+++ R/occ.r
@@ -86,12 +86,15 @@ foo_gbif <- function(sources, query, opts) {
         opts$taxonKey <- name_backbone(name = query)$usageKey
         opts$return <- "data"
         out <- do.call(occ_search, opts)
-        out$prov <- rep("gbif", nrow(out))
-        out$name <- as.character(out$name)
-        list(time = time, data = out)
-        # meta <- list(source='gbif', time=time, query=query, type=type, opts=opts)
+        if(class(out)=="character"){
+          list(time=time, data=data.frame(name=NA, key=NA, longitude=NA, latitude=NA, prov="gbif"))
+        } else
+        {
+          out$prov <- rep("gbif", nrow(out))
+          out$name <- as.character(out$name)
+          list(time=time, data=out)
+        }
     } else {
-        # meta <- list(source='gbif', time=NULL, query=NULL, type=NULL, opts=list())
         list(time = NULL, data = data.frame(NULL))
     }
     # list(meta=meta, data=out)

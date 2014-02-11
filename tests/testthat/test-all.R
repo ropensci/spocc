8c63af8dafa8caa432f779a9d590c4902bc496c4
Scott Chamberlain <myrmecocystus@gmail.com>
2014-02-11 11:50:23 -0800

    added tests for geometry searches

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -77,3 +77,24 @@ unlink("map/", recursive = TRUE)
 })
 
 
+context("Testing geometry searches")
+
+test_that("geometry searches work", {
+geo1 <- occ(query='Accipiter striatus', from='gbif', 
+    geometry='POLYGON((30.1 10.1, 10 20, 20 60, 60 60, 30.1 10.1))')
+geo2 <- occ(query='Accipiter striatus', from='gbif', geometry=c(-125.0,38.4,-121.8,40.9))
+geo3 <- occ(query='Accipiter striatus', from='ecoengine', limit=10, geometry=c(-125.0,38.4,-121.8,40.9))
+bounds <- c(-125.0,38.4,-121.8,40.9)
+geo4 <- occ(query = 'Danaus plexippus', from="inat", geometry=bounds)
+geo5 <- occ(query = 'Danaus plexippus', from=c("inat","gbif","ecoengine"), geometry=bounds)
+
+expect_is(geo1, "occdat")
+expect_is(geo2, "occdat")
+expect_is(geo3, "occdat")
+expect_is(geo4, "occdat")
+expect_is(geo5, "occdat")
+expect_equal(nrow(geo1$gbif$data[[1]]), 1)
+expect_match(names(geo2$gbif$data), 'Accipiter_striatus')
+expect_equal(nrow(geo4$gbif$data[[1]]), 0)
+expect_equal(ncol(occ2df(geo5)), 4)
+})

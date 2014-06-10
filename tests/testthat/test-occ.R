4e61f002ccee18f8638cba463d196ec16a3f5da4
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-10 09:40:34 -0700

    fixed occ tests

--- tests/testthat/test-occ.R
+++ tests/testthat/test-occ.R
@@ -65,33 +65,40 @@ expect_equal(unique(temp_df8$prov), "antweb")
 context("Testing geometry searches")
 
 test_that("geometry searches work", {
-geo1 <- occ(query='Accipiter striatus', from='gbif', 
-    geometry='POLYGON((30.1 10.1, 10 20, 20 60, 60 60, 30.1 10.1))')
-geo2 <- occ(query='Accipiter striatus', from='gbif', geometry=c(-125.0,38.4,-121.8,40.9))
-geo3 <- occ(query='Accipiter striatus', from='ecoengine', limit=10, geometry=c(-125.0,38.4,-121.8,40.9))
-bounds <- c(-125.0,38.4,-121.8,40.9)
-geo4 <- occ(query = 'Danaus plexippus', from="inat", geometry=bounds)
-geo5 <- occ(query = 'Danaus plexippus', from=c("inat","gbif","ecoengine"), geometry=bounds)
-
-expect_is(geo1, "occdat")
-expect_is(geo2, "occdat")
-expect_is(geo3, "occdat")
-expect_is(geo4, "occdat")
-expect_is(geo5, "occdat")
-expect_equal(nrow(geo1$gbif$data[[1]]), 1)
-expect_match(names(geo2$gbif$data), 'Accipiter_striatus')
-expect_equal(nrow(geo4$gbif$data[[1]]), 0)
-expect_equal(ncol(occ2df(geo5)), 4)
+  # no results
+  geo1 <- occ(query='Accipiter striatus', from='gbif', 
+              geometry='POLYGON((30.1 10.1, 10 20, 20 60, 60 60, 30.1 10.1))')
+  geo11 <- occ(query='Accipiter striatus', from='gbif', 
+              geometry=  
+  'POLYGON((-120.7 46.8,-103.1 46.4,-88.0 36.9,-109.5 32.6,-123.9 42.3,-120.7 46.8))')
+  
+  geo2 <- occ(query='Accipiter striatus', from='gbif', geometry=c(-125.0,38.4,-121.8,40.9))
+  geo3 <- occ(query='Accipiter striatus', from='ecoengine', limit=10, geometry=c(-125.0,38.4,-121.8,40.9))
+  bounds <- c(-125.0,38.4,-121.8,40.9)
+  geo4 <- occ(query = 'Danaus plexippus', from="inat", geometry=bounds)
+  geo5 <- occ(query = 'Danaus plexippus', from=c("inat","gbif","ecoengine"), geometry=bounds)
+  
+  expect_is(geo1, "occdat")
+  expect_is(geo2, "occdat")
+  expect_is(geo3, "occdat")
+  expect_is(geo4, "occdat")
+  expect_is(geo5, "occdat")
+  expect_equal(nrow(geo1$gbif$data[[1]]), 1)
+  expect_match(names(geo2$gbif$data), 'Accipiter_striatus')
+  expect_equal(nrow(geo4$gbif$data[[1]]), 0)
+  expect_equal(ncol(occ2df(geo5)), 4)
 })
 
 
 context("Testing by taxon identifier searches")
-library("taxize")
 
 test_that("Taxon identifier searches work", {
+  library("taxize")
   ids <- get_ids(names=c("Chironomus riparius","Pinus contorta"), db = c('itis','gbif'))
   byid1 <- occ(ids = ids[[1]], from='bison')
   byid2 <- occ(ids = ids, from=c('bison','gbif'))
+  byid2$bison
+  byid2$gbif
   
   ids <- get_ids(names="Chironomus riparius", db = 'gbif')
   byid3 <- occ(ids = ids, from='gbif')

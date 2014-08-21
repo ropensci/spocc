aa730a12aa07b6860da3f934000f4aacdda5638a
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-21 16:56:46 -0700

    added pacage level man file, closes #79

---
+++ R/spocc-package.R
@@ -0,0 +1,23 @@
+#' You don't need API keys for any of the data providers thus far. However, some
+#' may be added in the future that require authentication, just FYI.
+#'
+#' Currently supported species occurrence databases:
+#'
+#' \tabular{ll}{
+#' Provider \tab Web \cr
+#' GBIF \tab \url{http://www.gbif.org/} \cr
+#' BISON \tab \url{http://bison.usgs.ornl.gov/} \cr
+#' iNaturalist \tab \url{http://www.inaturalist.org/} \cr
+#' Berkeley ecoengine \tab \url{https://ecoengine.berkeley.edu/} \cr
+#' AntWeb \tab \url{http://www.antweb.org/} \cr
+#' }
+#'
+#' @name spocc-package
+#' @aliases spocc
+#' @docType package
+#' @title R interface to many species occurrence data sources
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @author Karthik Ram \email{karthik.ram@@gmail.com}
+#' @author Ted Hart \email{edmund.m.hart@@gmail.com}
+#' @keywords package
+NULL

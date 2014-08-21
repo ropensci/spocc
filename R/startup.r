3562ab5d0a0412b58064585bedc064f941210e6e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-21 16:45:28 -0700

    added startup message to package with vignette, citation, and mention to look at duplicates warning man page, #77

---
+++ R/startup.r
@@ -0,0 +1,3 @@
+.onAttach <- function(...) {
+  packageStartupMessage("\n\nNew to spocc? See the vignette at vignette('spocc_vignette', 'spocc')\nor at http://ropensci.org/tutorials/spocc_tutorial.html\ncitation(package='solr') for the citation for this package \nSee important note about duplicate records ?spocc_duplicates\nUse suppressPackageStartupMessages() to suppress this startup messages in the future\n")
+}

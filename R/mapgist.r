063e2a7ed4026d4d52badd7dcba4d25aff90398e
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-20 18:49:31 -0800

    using gistr now in gistmap fxn closes #100

--- R/mapgist.r
+++ R/mapgist.r
@@ -1,5 +1,8 @@
 #' Make an interactive map to view in the browser as a Github gist
 #' 
+#' @export
+#' @importFrom gistr gist_create
+#' 
 #' @param data A data.frame, with any number of columns, but with at least the 
 #'    following: name (the taxonomic name), latitude (in dec. deg.), longitude  
 #'    (in dec. deg.)
@@ -7,28 +10,15 @@
 #' @param file File name (without file extension) for your geojson file. Default is 'gistmap'.
 #' @param dir Directory for storing file and reading it back in to create gist. 
 #'    If none is given, this function gets your working directory and uses that.
+#' @param public (logical) Whether gist is public (default: TRUE)
 #' @param browse If TRUE (default) the map opens in your default browser.
 #' @param ... Further arguments passed on to \code{spocc_stylegeojson}
-#' @description 
-#' You will be asked ot enter you Github credentials (username, password) during
-#' each session, but only once for each session. Alternatively, you could enter
-#' your credentials into your .Rprofile file with the entries
 #' 
-#' \itemize{
-#'  \item options(github.username = 'your_github_username')
-#'  \item options(github.password = 'your_github_password')
-#' }
+#' @details See \code{\link[gistr]{gist_auth}} for help on authentication
 #' 
-#' then \code{mapgist} will simply read those options.
-#' 
-#' \code{mapgist} has modified code from the rCharts package by Ramnath Vaidyanathan 
-#' @return Creates a gist on your Github account, and prints out where the geojson file was
-#' written on your machinee, the url for the gist, and an embed script in the console.
-#' 
-#' @export
 #' @examples \dontrun{
 #' spp <- c('Danaus plexippus','Accipiter striatus','Pinus contorta')
-#' dat <- occ(spp, from=c('gbif','ecoengine'), gbifopts=list(hasCoordinate=TRUE))
+#' dat <- occ(spp, from=c('gbif','ecoengine'), limit=30, gbifopts=list(hasCoordinate=TRUE))
 #' dat <- fixnames(dat, "query")
 #' 
 #' # Define colors
@@ -43,19 +33,20 @@
 #' mapgist(data=df, symbol=c('park','zoo','garden'))
 #' }
 
-mapgist <- function(data, description = "", file = "gistmap", dir = NULL, browse = TRUE, 
-                    ...) 
-{
-  assert_that(is(data, "occdatind")|is(data, "occdat"))
-  data <- if(is(data, "occdatind")) do.call(rbind, data$data) else occ2df(data)
-  if (is.null(dir)) 
-    dir <- paste0(getwd(), "/")
+mapgist <- function(data, description = "", file = "gistmap", dir = NULL, 
+  public = TRUE, browse = TRUE, ...) {
+  
+  stopifnot(is(data, "occdatind") | is(data, "occdat"))
+  data <- if(is(data, "occdatind")) {
+    do.call(rbind, data$data) 
+  } else {
+    occ2df(data)
+  }
+  if (is.null(dir)) dir <- paste0(getwd(), "/")
   spplist <- as.character(unique(data$name))
   datgeojson <- spocc_stylegeojson(input = data, var = "name", ...)
   write.csv(datgeojson, paste(dir, file, ".csv", sep = ""))
   spocc_togeojson(input = paste(dir, file, ".csv", sep = ""), method = "web", destpath = dir, 
                   outfilename = file)
-  tt <- spocc_gist(paste(dir, file, ".geojson", sep = ""), description = description)
-  if (browse) 
-    browseURL(tt)
+  gist_create(paste(dir, file, ".geojson", sep = ""), description = description, public = public, browse = browse)
 } 

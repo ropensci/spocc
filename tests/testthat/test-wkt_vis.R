4a8af0e0d87ef5dce4c140408d08c9932ef4bfb7
Scott Chamberlain <myrmecocystus@gmail.com>
2015-09-21 14:40:08 -0700

    dont use non-exported fxn in wkt_vis eg added wkt_vis test

---
+++ tests/testthat/test-wkt_vis.R
@@ -0,0 +1,35 @@
+context("wkt_vis works")
+
+test_that("wkt_vis works with browse=FALSE", {
+  skip_on_cran()
+  
+  poly <- 'POLYGON((-111.06 38.84, -110.80 39.37, -110.20 39.17, -110.20 38.90,
+  -110.63 38.67, -111.06 38.84))'
+  aa <- wkt_vis(poly, browse = FALSE)
+  
+  expect_is(aa, "character")
+  expect_match(aa, ".html")
+  expect_match(aa, "spocc")
+  
+  aa_html <- paste0(readLines(aa), collapse = "\n")
+  expect_is(aa_html, "character")
+  expect_true(grepl("DOCTYPE", aa_html))
+  expect_true(grepl("L.mapbox", aa_html))
+})
+
+test_that("wkt_vis works with multiple polygons", {
+  skip_on_cran()
+  
+  x <- "POLYGON((-125 38.4, -121.8 38.4, -121.8 40.9, -125 40.9, -125 38.4), 
+   (-115 22.4, -111.8 22.4, -111.8 30.9, -115 30.9, -115 22.4))"
+  aa <- wkt_vis(x, browse = FALSE)
+  
+  expect_is(aa, "character")
+  expect_match(aa, ".html")
+  expect_match(aa, "spocc")
+  
+  aa_html <- paste0(readLines(aa), collapse = "\n")
+  expect_is(aa_html, "character")
+  expect_true(grepl("DOCTYPE", aa_html))
+  expect_true(grepl("L.mapbox", aa_html))
+})

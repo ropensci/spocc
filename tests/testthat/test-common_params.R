fa681765f4498ee1481f7c471b112140f9dc94ae
Scott Chamberlain <myrmecocystus@gmail.com>
2016-03-28 17:00:46 -0700

    use new testthat::expect_lt

--- tests/testthat/test-common_params.R
+++ tests/testthat/test-common_params.R
@@ -2,27 +2,27 @@ context("Testing global common parameters")
 
 test_that("limit", {
   skip_on_cran()
-  
+
   limit_1 <- occ(query = 'Accipiter', from = 'gbif', limit = 3)
   limit_2 <- occ(query = 'Accipiter striatus', from = 'gbif', limit = 5)
-  
+
   expect_is(limit_1, "occdat")
   expect_is(limit_2, "occdat")
-  
-  expect_less_than(NROW(limit_1$gbif$data$Accipiter), NROW(limit_2$gbif$data$Accipiter))
+
+  expect_lt(NROW(limit_1$gbif$data$Accipiter), NROW(limit_2$gbif$data$Accipiter))
 })
 
 test_that("geometry", {
   skip_on_cran()
-  
+
   # a full set of tests for this, so just briefly here
   bounds1 <- c(-120, 40, -100, 45)
   bounds2 <- c(-120, 40, -115, 42)
   geom_1 <- occ(from = "gbif", geometry = bounds1, limit = 5)
   geom_2 <- occ(from = "gbif", geometry = bounds2, limit = 5)
-  
+
   expect_is(geom_1, "occdat")
   expect_is(geom_2, "occdat")
-  
-  expect_less_than(geom_2$gbif$meta$found, geom_1$gbif$meta$found)  
+
+  expect_lt(geom_2$gbif$meta$found, geom_1$gbif$meta$found)
 })
--- tests/testthat/test-has_coords.R
+++ tests/testthat/test-has_coords.R
@@ -1,37 +1,37 @@
 context("Limit to records with coordinates via has_coords")
 
 no_ <- function(x) {
-  
+
 }
 
 test_that("has_coords works as expected", {
   skip_on_cran()
-  
+
   hc_1 <- occ(query = 'Accipiter', from = 'gbif', limit = 5, has_coords = TRUE)
   hc_2 <- occ(query = 'Accipiter striatus', from = 'gbif', limit = 5, has_coords = FALSE)
-  
+
   expect_is(hc_1, "occdat")
   expect_is(hc_2, "occdat")
-  
+
   expect_true(hc_1$gbif$meta$opts$hasCoordinate)
   expect_false(hc_2$gbif$meta$opts$hasCoordinate)
-  
-  expect_less_than(hc_2$gbif$meta$found, hc_1$gbif$meta$found)
+
+  expect_lt(hc_2$gbif$meta$found, hc_1$gbif$meta$found)
 })
 
 test_that("has_coords works with all data sources as planned", {
   skip_on_cran()
-  
+
   aa <- occ('Accipiter striatus', from = 'gbif', limit = 5, has_coords = TRUE)
   bb <- occ('Accipiter striatus', from = 'ecoengine', limit = 5, has_coords = TRUE)
   cc <- occ('linepithema humile', from = 'antweb', limit = 5, has_coords = TRUE)
   dd <- occ('Accipiter striatus', from = 'inat', limit = 5, has_coords = TRUE)
   ee <- occ('Accipiter striatus', from = 'idigbio', limit = 5, has_coords = TRUE)
   ff <- occ('Accipiter striatus', from = 'vertnet', limit = 5, has_coords = TRUE)
-  
+
   gg <- occ('Accipiter striatus', from = 'ebird', limit = 5, has_coords = TRUE)
   hh <- occ('Accipiter striatus', from = 'bison', limit = 5, has_coords = TRUE)
-  
+
   expect_is(aa, "occdat")
   expect_is(bb, "occdat")
   expect_is(cc, "occdat")
@@ -40,25 +40,25 @@ test_that("has_coords works with all data sources as planned", {
   expect_is(ff, "occdat")
   expect_is(gg, "occdat")
   expect_is(hh, "occdat")
-  
+
   expect_false(anyNA(aa$gbif$data[[1]]$longitude))
   expect_false(anyNA(aa$gbif$data[[1]]$latitude))
-  
+
   expect_false(anyNA(bb$ecoengine$data[[1]]$longitude))
   expect_false(anyNA(bb$ecoengine$data[[1]]$latitude))
-  
+
   expect_false(anyNA(cc$antweb$data[[1]]$longitude))
   expect_false(anyNA(cc$antweb$data[[1]]$latitude))
-  
+
   expect_false(anyNA(dd$inat$data[[1]]$longitude))
   expect_false(anyNA(dd$inat$data[[1]]$latitude))
-  
+
   expect_false(anyNA(ee$idigbio$data[[1]]$longitude))
   expect_false(anyNA(ee$idigbio$data[[1]]$latitude))
-  
+
   expect_false(anyNA(ff$vertnet$data[[1]]$longitude))
   expect_false(anyNA(ff$vertnet$data[[1]]$latitude))
-  
+
   expect_true(aa$gbif$meta$opts$hasCoordinate)
   expect_true(bb$ecoengine$meta$opts$georeferenced)
   expect_true(cc$antweb$meta$opts$georeferenced)

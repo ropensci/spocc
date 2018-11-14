74e3ac94ca5aab1deff501f762ecbf01425ea2de
Scott Chamberlain <myrmecocystus@gmail.com>
2018-11-14 11:47:05 -0800

    add assert() fxn for checking arg types

--- R/zzz.r
+++ R/zzz.r
@@ -107,3 +107,12 @@ rbindl <- function(x) {
                                                 use.names = TRUE))
   xx
 }
+
+assert <- function(x, y) {
+  if (!is.null(x)) {
+    if (!inherits(x, y)) {
+      stop(deparse(substitute(x)), " must be of class ",
+        paste0(y, collapse = ", "), call. = FALSE)
+    }
+  }
+}

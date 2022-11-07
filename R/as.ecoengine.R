e668999e75189aece70f0a8a38424b68f75693f0
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-11-07 12:50:51 +0100

    docs: fix for pkgdown

--- R/as.ecoengine.R
+++ R/as.ecoengine.R
@@ -1,8 +1,9 @@
 #' Coerce occurrence keys to ecoenginekey/occkey objects
-#' 
+#'
 #' DEFUNCT
 #'
 #' @export
 #' @rdname as.ecoengine-defunct
+#' @keywords internal
 #' @param ... ignored
 as.ecoengine <- function(...) .Defunct()
